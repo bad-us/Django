8ee771e3b17af9c5f0437eb510042cce164de935
Bad <bad-us@rambler.ru>
2021-05-31 19:00:15 +0300

    New repo

--- products/views.py
+++ products/views.py
@@ -8,3 +8,11 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+def test_context(request):
+    context = {
+        'title': 'Geekshop',
+        'header': 'Hello people!',
+        'username': 'Bad',
+    }
+    return render(request, 'products/test_context.html')
