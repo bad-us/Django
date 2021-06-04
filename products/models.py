6a9e5dac4ce39f2645372dc344524c423f578a5b
Bad <bad-us@rambler.ru>
2021-06-04 21:27:44 +0300

    Add media files

--- products/models.py
+++ products/models.py
@@ -5,13 +5,20 @@ from django.db import models
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    description=models.TextField(blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
-    image = models.ImageField(upload_to='products_images')
-    description = models.CharField(max_length=1024, blank=True,null=True)
-    short_description = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    description = models.CharField(max_length=1024, blank=True, null=True)
+    short_description = models.CharField(max_length=256, blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
