598c41e1525d9c49382e65b0265e1d24e51caef4
Bad <bad-us@rambler.ru>
2021-06-09 18:43:14 +0300

    Add form

--- users/views.py
+++ users/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+#from django.http import HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
+
 
 # Create your views here.
+
 def login(request):
-    context = {'title': 'Authorisation'
-    }
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'title': 'Authorisation',
+               'form': form
+               }
     return render(request, 'users/login.html', context)
+
 
 def register(request):
     context = {'title': 'Registration'
-    }
+               }
     return render(request, 'users/register.html', context)
