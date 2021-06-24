176c24a78dfc581bfebe63253e5f4aa9bc264f68
Bad <bad-us@rambler.ru>
2021-06-24 17:40:55 +0300

    Middle

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from products.views import products
 
 app_name = 'products'
 
-
-
 urlpatterns = [
     path('', products, name='index'),
+    path('<int:category_id>/', products, name='product'),
+    path('page/<int:page>/', products, name='page'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ import os.path
 
 from django.shortcuts import render
 from products.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 MODULE_DIR = os.path.dirname(__file__)
 
@@ -16,14 +17,34 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    context = {
-        'title': 'Products',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
-    # file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
-    # context['products'] = json.load(open(file_path, encoding='UTF-8'))
+# def products(request):
+#     context = {
+#         'title': 'Products',
+#         'products': Product.objects.all(),
+#         'categories': ProductCategory.objects.all(),
+#     }
+#     # file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
+#     # context['products'] = json.load(open(file_path, encoding='UTF-8'))
+#     return render(request, 'products/products.html', context)
+
+# def products(request, category_id=None):
+#     context = {'title': 'Products', 'categories': ProductCategory.objects.all()}
+#     if category_id:
+#         context['products'] = Product.objects.filter(category_id=category_id)
+#     else:
+#         context['products'] = Product.objects.all()
+#     return render(request, 'products/products.html', context)
+
+def products(request, category_id=None, page=1):
+    context = {'title': 'Products', 'categories': ProductCategory.objects.all()}
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
     return render(request, 'products/products.html', context)
-
-
