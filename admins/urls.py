3449e8513e09e9ddc3ca1fd739fc72907c4f456d
Bad <bad-us@rambler.ru>
2021-06-24 13:30:23 +0300

    Thats ALL

--- admins/urls.py
+++ admins/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from admins.views import index, admin_users, admin_users_update, admin_users_create, admin_users_delete
+from admins.views import index, admin_users, admin_users_update, admin_users_create, admin_users_delete, admin_users_active
+
 
 app_name = 'admins'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('users/create/', admin_users_create, name='admin_users_create'),
     path('users/update/<int:id>/', admin_users_update, name='admin_users_update'),
     path('users/delete/<int:id>/', admin_users_delete, name='admin_users_delete'),
+    path('users/delete/<int:id>/', admin_users_active, name='admin_users_active'),
 ]
--- baskets/admin.py
+++ baskets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from baskets.models import Basket
+
+
+class BasketAdmin(admin.TabularInline):
+    fields = ('product', 'created_timestamp', 'quantity')
+    readonly_fields = ('created_timestamp',)
+    model = Basket
+    extra = 0
 
 # Register your models here.
